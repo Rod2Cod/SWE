42568344ec0b747e202daa1194e7151cf60975f3
Maggion Alberto <74048267+Porror@users.noreply.github.com>
2025-03-27 12:02:28 +0100

    Aggiunta sistema di valutazione

--- Artificial_QI/src/application/evaluation/AlgoritmoValutazioneRisposteImpl.py
+++ Artificial_QI/src/application/evaluation/AlgoritmoValutazioneRisposteImpl.py
@@ -1,8 +1,21 @@
-from .AlgoritmoValutazioneRisposte import AlgoritmoValutazioneRisposte
+from AlgoritmoValutazioneRisposte import AlgoritmoValutazioneRisposte
+from Scorer import Scorer
+import pandas as pd
+import joblib
 
 class AlgoritmoValutazioneRisposteImpl(AlgoritmoValutazioneRisposte):
-    def __init__():
+    def __init__(self, model: str = "assets/model.joblib"):
+        self.__model = joblib.load(model)
+        self.__scorer = Scorer()
         pass
     
+    def __trandformInput(self, data: dict) -> pd.DataFrame:
+        return pd.DataFrame.from_dict(data, orient='index').T
+    
+
     def evaluate(self, risposta_attesa: str, risposta_llm: str) -> tuple[dict[str, float], float]:
-        pass
+        weights = self.__scorer.score(risposta_attesa, risposta_llm)
+        processedWeights = self.__trandformInput(weights)
+        score = self.__model.predict(processedWeights)
+
+        return(weights, score[0])
