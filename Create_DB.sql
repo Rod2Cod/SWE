324134946c406cea247c1a2303a72c1763417c9a
Filippo Bellon <filippo.bellon@studenti.unipd.it>
2025-04-01 16:11:17 +0200

    aggiornato SQL creazione database

--- Create_DB.sql
+++ Create_DB.sql
@@ -5,35 +5,34 @@ CREATE DATABASE Artificial_QI;
 -- Creazione delle tabelle
 
 CREATE TABLE ElementoDomanda (
-    id_domanda SERIAL PRIMARY KEY,
-    testo_domanda TEXT NOT NULL,
-    risposta_attesa TEXT NOT NULL
+    id SERIAL PRIMARY KEY,
+    domanda TEXT NOT NULL,
+    risposta TEXT NOT NULL
 );
 
 CREATE TABLE RisultatoTest (
-    id_risultato_test SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     LLM TEXT NOT NULL,
-    score_generale FLOAT NOT NULL,
-    data_test TIMESTAMP NOT NULL,
-    nome_set TEXT,
-    id_set INT NOT NULL,
+    score FLOAT NOT NULL,
+    data TIMESTAMP NOT NULL,
+    nomeSet TEXT,
     FOREIGN KEY (id_set) REFERENCES set_domande(id_set) ON UPDATE CASCADE
 );
 
 CREATE TABLE RisultatoSingolaDomanda (
-    id_risultato_domanda SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     domanda TEXT NOT NULL,
-    risposta_attesa TEXT NOT NULL,
-    risposta_LLM TEXT NOT NULL,
-    score_generale FLOAT NOT NULL,
-    id_risultato_test INT NOT NULL,
-    FOREIGN KEY (id_risultato_test) REFERENCES risultato_test(id_risultato_test) ON UPDATE CASCADE
+    risposta TEXT NOT NULL,
+    rispostaLLM TEXT NOT NULL,
+    score FLOAT NOT NULL,
+    risultatoTestId INT NOT NULL,
+    FOREIGN KEY (risultatoTestId) REFERENCES RisultatoTest(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE RisultatoMetrica (
-    nome_metrica TEXT NOT NULL,
-    id_risultato_domanda INT NOT NULL,
-    score_parziale FLOAT NOT NULL,
-    PRIMARY KEY (nome_metrica, id_risultato_domanda),
-    FOREIGN KEY (id_risultato_domanda) REFERENCES risultato_domanda(id_risultato_domanda) ON UPDATE CASCADE
+    nomeMetrica TEXT NOT NULL,
+    risultatoDomandaId INT NOT NULL,
+    score FLOAT NOT NULL,
+    PRIMARY KEY (nome_metrica, risultatoDomandaId),
+    FOREIGN KEY (risultatoDomandaId) REFERENCES RisultatoSingolaDomanda(id) ON UPDATE CASCADE
 );
