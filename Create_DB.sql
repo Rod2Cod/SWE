6da892900fd5a8d41d879934c4fa2ea0b739a299
Filippo Bellon <filippo.bellon@studenti.unipd.it>
2025-04-01 16:04:11 +0200

    aggiornato SQL DB

--- Create_DB.sql
+++ Create_DB.sql
@@ -1,29 +1,26 @@
 -- Creazione del database (PostgreSQL non supporta USE, quindi bisogna connettersi manualmente)
-CREATE DATABASE artificialqi;
+
+CREATE DATABASE Artificial_QI;
 
 -- Creazione delle tabelle
-CREATE TABLE set_domande (
-    id_set SERIAL PRIMARY KEY,
-    nome_set VARCHAR(100) NOT NULL
-);
 
-CREATE TABLE domanda (
+CREATE TABLE ElementoDomanda (
     id_domanda SERIAL PRIMARY KEY,
     testo_domanda TEXT NOT NULL,
     risposta_attesa TEXT NOT NULL
 );
 
-CREATE TABLE risultato_test (
+CREATE TABLE RisultatoTest (
     id_risultato_test SERIAL PRIMARY KEY,
-    LLM VARCHAR(200) NOT NULL,
+    LLM TEXT NOT NULL,
     score_generale FLOAT NOT NULL,
     data_test TIMESTAMP NOT NULL,
-    nome_set VARCHAR(100),
+    nome_set TEXT,
     id_set INT NOT NULL,
     FOREIGN KEY (id_set) REFERENCES set_domande(id_set) ON UPDATE CASCADE
 );
 
-CREATE TABLE risultato_domanda (
+CREATE TABLE RisultatoSingolaDomanda (
     id_risultato_domanda SERIAL PRIMARY KEY,
     domanda TEXT NOT NULL,
     risposta_attesa TEXT NOT NULL,
@@ -33,19 +30,10 @@ CREATE TABLE risultato_domanda (
     FOREIGN KEY (id_risultato_test) REFERENCES risultato_test(id_risultato_test) ON UPDATE CASCADE
 );
 
-
-CREATE TABLE metriche(
-    nome_metrica VARCHAR(100) NOT NULL,
+CREATE TABLE RisultatoMetrica (
+    nome_metrica TEXT NOT NULL,
     id_risultato_domanda INT NOT NULL,
     score_parziale FLOAT NOT NULL,
     PRIMARY KEY (nome_metrica, id_risultato_domanda),
     FOREIGN KEY (id_risultato_domanda) REFERENCES risultato_domanda(id_risultato_domanda) ON UPDATE CASCADE
-);
-
-CREATE TABLE domande_nei_set (
-    id_set INT NOT NULL ,
-    id_domanda INT NOT NULL,
-    PRIMARY KEY (id_set, id_domanda),
-    FOREIGN KEY (id_set) REFERENCES set_domande(id_set) ON UPDATE CASCADE,
-    FOREIGN KEY (id_domanda) REFERENCES domanda(id_domanda) ON UPDATE CASCADE
 );
