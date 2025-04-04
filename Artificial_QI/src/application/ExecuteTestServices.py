87c552df0b46de474b6f7e2564861b301d17f3cc
Filippo Bellon <filippo.bellon@studenti.unipd.it>
2025-04-04 17:23:03 +0200

    aggiornato service/usecase execute test e get status

--- Artificial_QI/src/application/ExecuteTestServices.py
+++ Artificial_QI/src/application/ExecuteTestServices.py
@@ -16,7 +16,7 @@ class ExecuteTestService(ExecuteTestUseCase):
         self.__saveTestport = save_port
         self.__getElementiDomanda_port = get_domande_port
 
-    def executeTest(self):
+    def executeTest(self) -> None:
         # recupero tutte le domande
 
         elementiDomanda = self.__getElementiDomanda_port.getAllElementiDomanda()
@@ -26,7 +26,6 @@ class ExecuteTestService(ExecuteTestUseCase):
         scores = []
         try:
             for elemento in elementiDomanda:
-
                 domanda = elemento.getDomanda().getText()
                 risposta = elemento.getRisposta().getText()
                 risposta_llm = self.__llm.makeQuestion(domanda)
@@ -40,19 +39,14 @@ class ExecuteTestService(ExecuteTestUseCase):
         except Exception as e:
             self.__status_tracker.set_error(str(e))
 
-
-
         score_totale = sum(scores) / len(scores)
 
         risultato_test = RisultatoTest(uuid.uuid1().int >> 64, score_totale, self.__llm.getName(),
                                        datetime.datetime.now(), None, risultati)
 
-
         risultato = self.__saveTestport.saveRisultatoTest(risultato_test)
         self.__status_tracker.set_id_risultato(risultato.getId())
         self.__status_tracker.finish_test()
-
-
 
 class GetTestStatusService(GetTestStatusUseCase):
     def __init__(self, status_tracker: TestStatusTracker):
